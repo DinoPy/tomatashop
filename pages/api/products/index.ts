5aaf05c4ef97c1a2565ce6cedd4264a31e429ed4
DinoPy <al3x.dinoiu@gmail.com>
2022-08-29 11:18:43 +0300

    changed .ts from import file

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,38 +1,38 @@
-import dbConnect from "../../../lib/dbConnect.js";
-import type { NextApiRequest, NextApiResponse } from "next";
-import Products from "../../../models/Products.ts";
+import dbConnect from '../../../lib/dbConnect.js';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Products from '../../../models/Products';
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-  const { method } = req;
+	const { method } = req;
 
-  await dbConnect();
+	await dbConnect();
 
-  switch (method) {
-    case "GET":
-      res.status(200).json({
-        message: "Get all products",
-        products: await Products.find(),
-      });
-      break;
-    case "POST":
-      const { title, price, description, image, category, rating } = req.body;
-      if (!title || !price || !description || !image || !category || !rating) {
-        res.status(400).json({ message: "Please provide all fields" });
-      }
-      res.status(200).json({
-        message: "Create new product",
-        product: await Products.create(req.body),
-      });
-      break;
-    default:
-      res.status(405).json({
-        message: "Method not allowed",
-      });
-      break;
-  }
+	switch (method) {
+		case 'GET':
+			res.status(200).json({
+				message: 'Get all products',
+				products: await Products.find(),
+			});
+			break;
+		case 'POST':
+			const { title, price, description, image, category, rating } = req.body;
+			if (!title || !price || !description || !image || !category || !rating) {
+				res.status(400).json({ message: 'Please provide all fields' });
+			}
+			res.status(200).json({
+				message: 'Create new product',
+				product: await Products.create(req.body),
+			});
+			break;
+		default:
+			res.status(405).json({
+				message: 'Method not allowed',
+			});
+			break;
+	}
 }
 
 //  todo: complete implemntation of typescript in schema and model.
