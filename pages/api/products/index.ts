8afb06ff0260eccc36f6ea1dcb9b5025ac87d8b8
DinoOne <001test.alex@gmail.com>
2022-08-29 22:04:00 +0300

    added PUT to products and error handling (all)

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,42 +1,90 @@
-import dbConnect from '../../../lib/dbConnect.js';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import Products from '../../../models/Products';
+import dbConnect from "../../../lib/dbConnect.js";
+import type { NextApiRequest, NextApiResponse } from "next";
+import Products from "../../../models/Products";
+
+// TO DO - add pagination
+// todo - add search
+// todo - add sorting
+// todo - add filtering
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-	const { method } = req;
+  const { method } = req;
 
-	await dbConnect();
+  try {
+    await dbConnect();
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
 
-	switch (method) {
-		case 'GET':
-			res.status(200).json({
-				message: 'Get all products',
-				products: await Products.find(),
-			});
-			break;
+  switch (method) {
+    case "GET": {
+      try {
+        const products =
+          await Products.find(); /* find all the data in our database */
+        res.status(200).json({ success: true, data: products });
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(400).json({ success: false, error: error.message });
+        }
+      }
+      break;
+    }
 
-		case 'POST':
-			const { title, price, description, image, category, rating } = req.body;
-			if (!title || !price || !description || !image || !category || !rating) {
-				res.status(400).json({ message: 'Please provide all fields' });
-			}
-			res.status(200).json({
-				message: 'Create new product',
-				product: await Products.create(req.body),
-			});
-			break;
+    case "POST": {
+      const { title, price, description, image, category, rating } = req.body;
+      if (!title || !price || !description || !image || !category || !rating) {
+        res.status(400).json({ message: "Please provide all fields" });
+      }
+      try {
+        const product = await Products.create({
+          title,
+          price,
+          description,
+          image,
+          category,
+          rating,
+        });
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(400).json({ success: false, error: error.message });
+        }
+      }
+      break;
+    }
 
-		default:
-			res.status(405).json({
-				message: 'Method not allowed',
-			});
-			break;
-	}
+    case "PUT": {
+      const { id } = req.query;
+      const { title, price, description, image, category, rating } = req.body;
+      if (!id) {
+        res.status(400).json({ message: "Please provide an id" });
+      }
+      try {
+        const product = await Products.findByIdAndUpdate(id, {
+          title,
+          price,
+          description,
+          image,
+          category,
+          rating,
+        });
+        res.status(200).json({ success: true, data: product });
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(400).json({ success: false, error: error.message });
+        }
+      }
+      break;
+    }
+
+    default:
+      res.status(405).json({
+        message: "Method not allowed",
+      });
+      break;
+  }
 }
-
-//  todo: complete implemntation of typescript in schema and model.
-
-//  todo: add pagination
